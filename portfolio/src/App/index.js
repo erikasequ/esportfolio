64b842917dd8ef896aaef11c425d97c4b9a99040
erikasequ <erikasequ@gmail.com>
2019-03-15 10:28:08 -0400

    first commit

---
+++ portfolio/src/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./style.css";
+import Bio from "../Components/Bio"
+import Project from "../Components/Projects"
+
+class App extends Component {
+  render() {
+    return (
+      <Bio />
+    )
+  }
+}
+
+export default App;
---
+++ portfolio/src/Components/Bio.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Bio = () => {
+    return ( 
+        <div className="photo">
+            <image></image>
+            <h1>Erika Sequeira</h1>
+            <h3>Full Stack Web Developer</h3>
+            <h4>e: erikasequ@gmail.com</h4>
+        </div>
+     );
+}
+ 
+export default Bio;
