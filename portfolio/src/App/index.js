c63528f5340f8a90eda85098cf0b61d786fd759f
erikasequ <erikasequ@gmail.com>
2019-03-15 11:00:14 -0400

    images being added

--- portfolio/src/App/index.js
+++ portfolio/src/App/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import "./style.css";
 import Bio from "../Components/Bio"
-import Project from "../Components/Projects"
+import Projects from "../Components/Projects"
 
 class App extends Component {
   render() {
     return (
+      <div>
       <Bio />
+      <Projects />
+      </div>
     )
   }
 }
---
+++ portfolio/src/Components/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import dailyaxis from "../Assets/dailyaxis.png"
+
+
+const Projects = () => {
+    return ( 
+        <img src={dailyaxis}/>
+     );
+}
+ 
+export default Projects;
